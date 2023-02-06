9cd6b615cf1031de130b5381e3c0386189db9d5b
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-06 18:31:53 +0100

    lineChart

--- src/components/Charts/LineCharts.jsx
+++ src/components/Charts/LineCharts.jsx
@@ -16,9 +16,19 @@ import {
   LinePrimaryAxis,
   LinePrimaryXAxis,
 } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 const LineChart = () => {
+  const { currentMode } = useStateContext();
   return (
-    <ChartComponent>
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryXAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373e" : "#fff"}
+    >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip, DateTime]} />
       <SeriesCollectionDirective>
         {lineCustomSeries.map((item, index) => (
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -5,7 +5,7 @@ const Line = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="Chart" title="inflation Rate" />
-      <div className="w-full">
+      <div className="w-full mt-10">
         <LineChart />
       </div>
     </div>
