feb2494a14cdd7b1dcd4e131de35d10c0c03ed2d
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-06 01:19:33 +0100

    Update ColorPicker.jsx

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -21,7 +21,7 @@ const CustomColorPicker = ({ id, mode }) => (
 
 const ColorPicker = () => (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-    <Header category="App" title="Co  lor Picker" />
+    <Header category="App" title="Color Picker" />
     <div className="text-center">
       <div id="preview" />
       <div className="flex justify-center items-center gap-20 flex-wrap">
